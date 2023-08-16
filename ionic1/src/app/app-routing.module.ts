29871e3110ba41016edbf2348d13164298b08e57
eSponjosa <e.rojascisterna@outlook.com>
2023-08-16 13:40:50 -0400

    generar pagina y cambiar path inicial

--- ionic1/src/app/app-routing.module.ts
+++ ionic1/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'uno',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'uno',
+    loadChildren: () => import('./uno/uno.module').then( m => m.UnoPageModule)
   },
 ];
 
