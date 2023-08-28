edfd597941c07e423f345d334b9c6a3c8a042fa3
DonChapman28 <e.rojascisterna@outlook.com>
2023-08-28 15:08:07 -0400

    pc duoc
    inputs y control de variables de ts

---
+++ ionic1/src/app/seis/seis.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seis',
+  templateUrl: './seis.page.html',
+  styleUrls: ['./seis.page.scss'],
+})
+export class SeisPage implements OnInit {
+  nombre : string;
+  edad : number;
+  mensaje : string = "Premium";
+
+  asignaturas = [
+                {nombre:"base de datos", nota:50},
+                {nombre:"programacion", nota:45},
+                {nombre:"matematica", nota:33},
+  ]
+  constructor() { 
+    this.nombre = "juan perez";
+    this.edad = 30;
+    console.log(this.asignaturas);
+    console.table(this.asignaturas);
+  
+
+
+  }
+
+  ngOnInit() {
+  }
+
+}
