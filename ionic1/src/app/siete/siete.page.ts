f0eec047970cce955bf4011f895366e4c2e4bb3e
DonChapman28 <e.rojascisterna@outlook.com>
2023-09-04 14:58:14 -0300

    captura de datos y ejercicio
    captura de datos y ejercicio de sumar. terminar ejercicios, crear calculadora que permita promediar 3 notas, mosrar si aprobo y usar si reprueba y toast si aprueba

--- ionic1/src/app/siete/siete.page.ts
+++ ionic1/src/app/siete/siete.page.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-siete',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./siete.page.scss'],
 })
 export class SietePage implements OnInit {
+//string, number, boolean, any
+nombre = 'Juan';
+apellidoPaterno : string;
+apellidoMaterno : string = "Fuentes"; 
+num1 = '';
+num2 = '';
+total : any = '';
+  constructor(private toastController: ToastController) { 
+    this.apellidoPaterno = "Diaz";
 
-  constructor() { }
+
+  }
+  
+
 
   ngOnInit() {
+
+
   }
+
+  async sumar()
+  {
+    this.total = this.num1 + this.num2;
+    const toast = await this.toastController.create({
+      message: 'El resultado es '+ this.total ,
+      duration : 3000,
+      position : 'bottom',
+    });
+    await toast.present();
+  }  
+  
 
 }
